f4df16cac9d2e4b269ad54f8ef3016801286d4ef
Dallas Carraher <dallascarraher@10-249-247-83.wireless.oregonstate.edu>
2019-05-28 11:57:34 -0700

    fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
 import Navbar from './Components/Navbar'
 import PostForm from './Components/PostForm'
 import Home from './Components/Home'
 import Manage from './Components/Manage'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-=======
 import PostForm from './PostForm/PostForm'
->>>>>>> 2926f28a22faf101cfa294c12f2530dbef7fb7b3
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Router>
       <div className="App">
         <Navbar />
@@ -24,11 +20,6 @@ class App extends Component {
         </Switch>
       </div>
       </Router>
-=======
-      <div className="App">
-        <PostForm />
-      </div>
->>>>>>> 2926f28a22faf101cfa294c12f2530dbef7fb7b3
     )
   }
 }
